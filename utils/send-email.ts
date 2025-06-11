541c3481dcaf24962c8a600e36537ec635760ec7
ashmit-p <ashmitp1012@gmail.com>
2025-06-11 11:21:41 +0530

    Update send-email.ts

--- utils/send-email.ts
+++ utils/send-email.ts
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
+
 
 import { FormData } from '@/components/ContactUs';
 
 export function sendEmail(data: FormData) {
   // TODO: send email
   console.log(data);
-=======
-
-import { FormData } from '@/components/ContactUs';
-
-export function sendEmail(data: FormData) {
-  // TODO: send email
-  console.log(data);
->>>>>>> 45668a71b3855042b0de6ca95fba425a741a3140
 }
